a5b4e52c7eff6ec9b6d1b11ada949e9f404ad49a
Jose Wennas <josewennas@hotmail.com>
2026-01-19 11:10:58 -0300

    Resetando a Classe

--- src/main/java/dev/josewennas/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/josewennas/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -10,31 +10,5 @@ public class CadastroDeNinjasApplication {
 		SpringApplication.run(CadastroDeNinjasApplication.class, args);
 	}
 
-    //olá Wennas!
-    int numero = 6;
-    int soma;
-    public int soma(){
-        return soma = numero + numero;
-    }
-
-    int divisao;
-
-    public int divisao(){
-        return divisao = numero / 2 ;
-    }
-
-    int multiplicacao;
-
-    public int multiplicacao(){
-        return multiplicacao = numero * 2;
-    }
-
-    public int maiorMenor(){
-        if(multiplicacao > 0){
-            return multiplicacao;
-        }else {
-            return soma;
-        }
-    }
 
 }
