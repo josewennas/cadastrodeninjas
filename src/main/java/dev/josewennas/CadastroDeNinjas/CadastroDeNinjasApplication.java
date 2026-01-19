9e16e0583d425e24655151c3670a8aae6487aa96
Jose Wennas <josewennas@hotmail.com>
2026-01-19 18:37:57 -0300

    Executando mensagem de boas vindas

--- src/main/java/dev/josewennas/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/josewennas/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CadastroDeNinjasApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroDeNinjasApplication.class, args);
 	}
-
-
 }
